91d13c6879dda35c4d0b14f89bb605d65b28acbe
Jacob Little <Jacob.Little@asos.com>
2023-09-25 09:32:11 +0100

    add new tests

--- TicTacToe.UnitTests/BoardClassShould.cs
+++ TicTacToe.UnitTests/BoardClassShould.cs
@@ -29,17 +29,28 @@ namespace TicTacToe.UnitTests
         }
 
         [Test]
-
-        public void WhenPlayerHasThreeInRowThePlayerWins()
+        public void WhenTwoMovesAreMadeTheBoardIsUpdated()
         {
             var board = new Board();
 
-            board.AddToBoard(0, 'X');
             board.AddToBoard(1, 'X');
-            board.AddToBoard(2, 'X');
+            board.AddToBoard(2, 'O');
+            var visualBoard = board.GetVisualBoard();
 
-            
+            Assert.That(visualBoard, Is.EqualTo("   | X | O \n   |   |   \n   |   |   "));
+        }
 
+        [Test]
+        public void WhenThreeMovesAreMadeTheBoardIsUpdated()
+        {
+            var board = new Board();
+
+            board.AddToBoard(1, 'X');
+            board.AddToBoard(2, 'O');
+            board.AddToBoard(0, 'X');
+            var visualBoard = board.GetVisualBoard();
+
+            Assert.That(visualBoard, Is.EqualTo(" X | X | O \n   |   |   \n   |   |   "));
         }
     }
 }
