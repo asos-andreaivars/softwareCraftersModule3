dee3e9faedcca68cf30cd2d9bbecfb32bad42930
Jacob Little <Jacob.Little@asos.com>
2023-09-25 09:51:02 +0100

    add new failing test

--- TicTacToe.UnitTests/BoardClassShould.cs
+++ TicTacToe.UnitTests/BoardClassShould.cs
@@ -5,16 +5,19 @@ namespace TicTacToe.UnitTests
 {
     public class BoardClassShould
     {
+        private Board _sut;
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Board();
+        }
 
         [Test]
         public void CreateAnEmptyBoardGivenNoMovesHaveBeenMade()
         {
-            var board = new Board();
-
-            var visualBoard = board.GetVisualBoard();
+            var visualBoard = _sut.GetVisualBoard();
 
             Assert.That(visualBoard, Is.EqualTo("   |   |   \n   |   |   \n   |   |   "));
-
         }
 
         [TestCase(new char[] { 'X' }, new int[] { 1 }, "   | X |   \n   |   |   \n   |   |   ")]
@@ -22,16 +25,25 @@ namespace TicTacToe.UnitTests
         [TestCase(new char[] { 'X', 'O', 'X' }, new int[] { 1, 2, 0 }, " X | X | O \n   |   |   \n   |   |   ")]
         public void WhenMovesAreMadeTheBoardIsUpdated(char[] players, int[] positions, string result)
         {
-            var board = new Board();
-
             for(int i = 0; i < players.Length; i++)
             {
-                board.AddToBoard(positions[i], players[i]);
+                _sut.AddToBoard(positions[i], players[i]);
             }
 
-            var visualBoard = board.GetVisualBoard();
+            var visualBoard = _sut.GetVisualBoard();
 
             Assert.AreEqual(result, visualBoard.ToString());
+        }
+
+        [Test]
+        public void WhenMoveIsMadeAndPlayerHasAlreadyPlayedThereThenMoveNotMade()
+        {
+            _sut.AddToBoard(0, 'X');
+            _sut.AddToBoard(0, 'O');
+
+            var visualBoard = _sut.GetVisualBoard();
+
+            Assert.AreEqual("   | X |   \n   |   |   \n   |   |   ", visualBoard.ToString());
         }
     }
 }
